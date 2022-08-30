e3dff2e3008a27da8f79d817e0ff4c9bf843e340
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-31 03:14:53 +0530

    create a VehicleService.java and define main method

---
+++ src/main/java/com/vehicles/VehicleService.java
@@ -0,0 +1,12 @@
+/*
+ * Author Name: Mohit Saini
+ * Date: 31-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.vehicles;
+
+public class VehicleService {
+    public static void main ( String[] args ) {
+    
+    }
+}
