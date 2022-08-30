483aad8a5e9cb296b5aa8d1ac2079b715f48a276
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-31 03:27:41 +0530

    final commit

--- src/main/java/com/vehicles/VehicleService.java
+++ src/main/java/com/vehicles/VehicleService.java
@@ -7,7 +7,12 @@ package com.vehicles;
 
 public class VehicleService {
     public static void main ( String[] args ) {
-        Bike bike = new Bike ( );
+        Bike bike = new Bike ( "Bajaj Pulsar" , 766383 , "sportsBike" );
+        Car car = new Car ( "HondaCity" , 426473813 , "Sedan" );
+        System.out.println ( "Bike Type is = " + bike.getVehicleType ( ) + "Its Speed is = " + bike.maxSpeed ( bike.getVehicleType ( ) ) );
+        System.out.println ( "ManufactureInformation =" + bike.getManufacturerInformation ( ) );
+        System.out.println ( "Car Type is =" + car.getVehicleType ( ) + "Its Speed is = " + car.maxSpeed ( car.getVehicleType ( ) ) );
+        System.out.println ( "ManufactureInformation =" + car.getManufacturerInformation ( ) );
     
     }
 }
